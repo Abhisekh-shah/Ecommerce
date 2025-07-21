2f590d282b4df206fa9e39e3f3794ffb123714fc
Abhisekh_shah <abhisekhshah9@gmail.com>
2025-07-21 23:59:07 +0545

    Auth

--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -2,14 +2,11 @@ import React, { useEffect, useState } from "react";
 import { backendUrl, currency } from "../App";
 import axios from "axios";
 import { toast } from "react-toastify";
-function List({ token }) {
+function List() {
   const [list, setList] = useState([]);
   const fetchList = async () => {
     try {
-      const response = await axios.get(backendUrl + "/api/product/list", {
-        headers: { token },
-      });
-
+      const response = await axios.get(backendUrl + "/api/product/list")
       if (response.data.success) {
         setList(response.data.products);
       } else {
